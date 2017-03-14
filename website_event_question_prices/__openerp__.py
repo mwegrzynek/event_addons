afdfcfbe3d525a58a296eb6c6f0df28fc007f450
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2017-03-14 07:10:02 +0100

    Initial commit

---
+++ website_event_question_prices/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Event question pricing (ticket configurator)',
+    'version': '1.0.0',
+    'category': 'Marketing',
+    'description': 'Adds prices to answers to questions to create a ticket configurator',
+    'author': 'Michał Węgrzynek',
+    'website': 'https://malloc.com.pl',
+    'depends': [
+        'event_sale',
+        'website_event_questions'
+    ],
+    'data': [
+
+    ],
+    'demo': [
+        'data/demo.xml'
+    ],
+    'installable': True,
+    'auto_install': False
+}
